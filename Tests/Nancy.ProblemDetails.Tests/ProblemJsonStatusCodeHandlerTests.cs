e59c727154b27472dbf3d34dbabb8b0c58c8fc57
tpluscode <tpluscode@users.noreply.github.com>
2016-11-11 14:23:52 +0100

    add status code handler

---
+++ Tests/Nancy.ProblemDetails.Tests/ProblemJsonStatusCodeHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using Shouldly;
+using Tavis;
+using Xunit;
+
+namespace Nancy.ProblemDetails.Tests
+{
+    public class ProblemJsonStatusCodeHandlerTests
+    {
+        private readonly ProblemJsonStatusCodeHandlerTestable _handler;
+
+        public ProblemJsonStatusCodeHandlerTests()
+        {
+            _handler = new ProblemJsonStatusCodeHandlerTestable();
+        }
+
+        [Fact]
+        public void When_not_set_up_to_handle_status_code_HandlesStatusCode_Should_return_false()
+        {
+            // when
+            var handlesStatusCode = _handler.HandlesStatusCode(HttpStatusCode.BadGateway, new NancyContext());
+
+            // then
+            handlesStatusCode.ShouldBeFalse();
+        }
+
+        [Fact]
+        public void When_set_up_to_handle_status_code_HandlesStatusCode_Should_return_true()
+        {
+            // given
+            _handler.SetHandler(HttpStatusCode.BadGateway);
+
+            // when
+            var handlesStatusCode = _handler.HandlesStatusCode(HttpStatusCode.BadGateway, new NancyContext());
+
+            // then
+            handlesStatusCode.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void When_not_set_up_to_handle_status_code_Handle_should_set_ProblemJsonResponse_with_returned_ProblemDocument()
+        {
+            // given
+            var nancyContext = new NancyContext();
+            var problemDocument = new ProblemDocument();
+            _handler.SetHandler(HttpStatusCode.BadGateway, problemDocument);
+
+            // when
+            _handler.Handle(HttpStatusCode.BadGateway, nancyContext);
+
+            // then
+            nancyContext.Response.ShouldBeOfType<ProblemJsonResponse>();
+            ((ProblemJsonResponse)nancyContext.Response).ProblemDocument.ShouldBeSameAs(problemDocument);
+        }
+
+        [Fact]
+        public void When_set_up_to_handle_status_code_Handle_should_set_ProblemJsonResponse_with_unchanged_code()
+        {
+            // given
+            var nancyContext = new NancyContext();
+            var problemDocument = new ProblemDocument();
+            _handler.SetHandler(HttpStatusCode.BadGateway, problemDocument);
+
+            // when
+            _handler.Handle(HttpStatusCode.BadGateway, nancyContext);
+
+            // then
+            nancyContext.Response.StatusCode.ShouldBe(HttpStatusCode.BadGateway);
+        }
+
+        [Fact]
+        public void When_set_up_to_handle_status_code_Handle_should_set_ProblemDocument_status_code()
+        {
+            // given
+            var nancyContext = new NancyContext();
+            var problemDocument = new ProblemDocument();
+            _handler.SetHandler(HttpStatusCode.BadGateway, problemDocument);
+
+            // when
+            _handler.Handle(HttpStatusCode.BadGateway, nancyContext);
+
+            // then
+            ((ProblemJsonResponse)nancyContext.Response).ProblemDocument.StatusCode.ShouldBe(System.Net.HttpStatusCode.BadGateway);
+        }
+
+        private class ProblemJsonStatusCodeHandlerTestable : ProblemJsonStatusCodeHandler
+        {
+            public void SetHandler(HttpStatusCode httpStatusCode, ProblemDocument problemDocument = null)
+            {
+                problemDocument = problemDocument ?? new ProblemDocument();
+                When((code, context) => code == httpStatusCode, context => problemDocument);
+            }
+        }
+    }
+}
---
+++ src/Nancy.ProblemDetails/ProblemJsonStatusCodeHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Nancy.ErrorHandling;
+using Tavis;
+
+namespace Nancy.ProblemDetails
+{
+    public delegate bool ShouldHandleStatusCode(HttpStatusCode code, NancyContext context);
+
+    public delegate ProblemDocument ProblemDocumentFactry(NancyContext context);
+
+    public abstract class ProblemJsonStatusCodeHandler : IStatusCodeHandler
+    {
+        private readonly ICollection<Handler> _handlers = new List<Handler>();
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return _handlers.Any(handler => handler.ShouldHandleStatusCode(statusCode, context));
+        }
+
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            var handler = _handlers.First(h => h.ShouldHandleStatusCode(statusCode, context));
+
+            var problemDocument = handler.ProblemDocumentFactory(context);
+            problemDocument.StatusCode = (System.Net.HttpStatusCode)statusCode;
+            context.Response = new ProblemJsonResponse(problemDocument).WithStatusCode(statusCode);
+        }
+
+        protected void When(ShouldHandleStatusCode shouldhandleStatusCode, ProblemDocumentFactry problemDocumentFactory)
+        {
+            _handlers.Add(new Handler
+            {
+                ShouldHandleStatusCode = shouldhandleStatusCode,
+                ProblemDocumentFactory = problemDocumentFactory
+            });
+        }
+
+        private class Handler
+        {
+            public ShouldHandleStatusCode ShouldHandleStatusCode { get; set; }
+            public ProblemDocumentFactry ProblemDocumentFactory { get; set; }
+        }
+    }
+}
